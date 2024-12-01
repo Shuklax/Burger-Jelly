1965cac3d46929db1c4a1c6fc12a53db1da37d61
Shuklax <harshshuklaknp@gmail.com>
2024-12-01 19:25:32 +0530

    fetch the data from live API; Build a shimmer UI

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,26 +1,47 @@
-import RestaurantCard from "./RestaurandCard";
+import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "../../App.css";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(()=>{
+    fetchData();
+  }, [])
+
+  const fetchData =  async() => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.49690&lng=80.32460&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+    const jsonData = await data.json();
+    console.log(jsonData);
+
+    setListOfRestaurants(jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  }
+
+  if(listOfRestaurants.length===0){
+    return <Shimmer/>;
+  }
 
   return (
     <div className="body">
-      <div className="search">
-        <input type="text" placeholder="search....." />
-      </div>
-      <div className="filter">
-        <button
-          onClick={() => {
-            const newListOfRestaurants = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4.3
-            );
-            setListOfRestaurants(newListOfRestaurants);
-          }}
-        >
-          Top Rated Restaurants
-        </button>
+      <div className="body-header">
+        <div className="search">
+          <input type="text" placeholder="search....." />
+        </div>
+        <div className="filter">
+          <button
+            onClick={() => {
+              const newListOfRestaurants = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4.3
+              );
+              setListOfRestaurants(newListOfRestaurants);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
       </div>
       <div className="restaurantContainer">
         {listOfRestaurants.map((restaurant) => (

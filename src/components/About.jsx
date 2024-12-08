fab21be9d24197de00ea66bfc88a0e1363cb65ac
Shuklax <harshshuklaknp@gmail.com>
2024-12-08 22:03:39 +0530

    experiment with class components and lifesycle methods in the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,34 @@
+import { Component } from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("parent constructor");
+  }
+
+  componentDidMount() {
+    console.log("parent mounted");
+  }
+
+  render() {
+    console.log("parent render");
     return (
-        <div className="AboutUs">
-            <h1>ABOUT US</h1>
-            <h2>this is the about us page</h2>
-            <User name={"Harsh Shukla (function)"} location={"Mangalore"} contact={"Shuklax@Github"}/>
+      <div className="AboutUs">
+        <h1>ABOUT US</h1>
+        <h2>this is the about us page</h2>
+        {/* <User name={"Harsh Shukla (function)"} location={"Mangalore"} contact={"Shuklax@Github"}/> */}
 
+        <UserClass
+          name={"Harsh Shukla (class)"}
+          location={"Mangalore"}
+          contact={"Shuklax@Github"}
+        />
 
-            <UserClass name={"Harsh Shukla (class)"} location={"Mangalore"} contact={"Shuklax@Github"}/>
-        </div>
-    )
+      </div>
+    );
+  }
 }
 
 export default About;
